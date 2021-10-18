c4cfee2fe576519312b46356273fa744516de9f0
PyroCandy <71510505+PyroCandy@users.noreply.github.com>
2021-10-18 12:58:39 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ let client = new sdk.Client();
 let database = new sdk.Database(client);
 
 client
-    .setEndpoint('http://localhost/v1')
-    .setProject('616c1002bde30')
-    .setKey('e38d9d4e7dadf7d913adb83aaaba3614ebdbb5b5c9001fc3133259895763583d7ebc260c456b849ea3a2a685645c427b85fc8eab201cb72696d07cc4a0a5952b5e337b08155227058d00b25aca63771ec8c7bee90125ef3466fb9ef3dad1fc2fc67e88ba6dd0827990fbb43c74b26cd651539e6d7230cc79f67ec08fa67cdd16') // Your secret API key
+    .setEndpoint('[API_Endpoint]')
+    .setProject('[PROJECT_ID]')
+    .setKey('[API_KEY]') // Your secret API key
 ;
 
 app.get('/', (req, res) => {
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
  });
  
 app.get('/pokemon', async (req,res) => {
-    let data = await database.listDocuments('616c1330d96ee');
+    let data = await database.listDocuments('[COLLECTION_ID]');
     res.send(data.documents);
 })
 
